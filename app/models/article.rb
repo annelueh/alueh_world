fea752a6a31f159c7c05ef149df8b8d70ff122e5
Jacqueline Pan <jacquelp@andrew.cmu.edu>
2016-02-15 11:34:46 -0500

    relationships, required fields, scopes

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ActiveRecord::Base
+	belongs_to :category
+	validates_presence_of :title, :content
+	
+	# Scopes
+ 	scope :alphabetical, -> { order('title') }
+ 	scope :active, -> { where(active: true) }
+end
